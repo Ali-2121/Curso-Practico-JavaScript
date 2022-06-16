1a534ef10e7c254c9c5bd76daa8a4e0fe0d70165
Ali-2121 <prds20bnll@gmail.com>
2022-06-16 14:43:02 -0500

    creamos arrays y un objeto para calcular la moda dentro de una lista de varios números que se repiten

---
+++ moda.js
@@ -0,0 +1,37 @@
+const lista1 = [
+    1,
+    3,
+    3,
+    5,
+    6,
+    9,
+    9,
+    3,
+    6,
+    1,
+    6,
+    9,
+    9,
+    9,
+];
+const lista1Count = {};
+
+lista1.map(
+    function ( elemento){
+        if (lista1Count[elemento] ){
+            lista1Count[elemento] += 1;
+        }else{
+            lista1Count[elemento] = 1;
+        }
+        
+    }
+)
+
+
+const lista1Array =Object.entries(lista1Count).sort(
+    function (elementoA, elementoB) {
+        return elementoA[1] - elementoB[1];
+    }
+)
+
+const moda = lista1Array[lista1Array.length - 1];
